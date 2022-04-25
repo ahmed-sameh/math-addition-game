bb87c673f7aa4f9af465f9b00c398633a3c56444
ahmed-sameh <medo5love55@gmail.com>
2022-04-25 21:38:52 +0200

    assignment finshed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,8 @@ export class AppComponent implements OnInit {
   secondNum = 0;
   result = 0;
   correctAnswer = true;
-  startTime!: number;
-  endTime!: number;
+  startAnswer!: number;
+  finshAnswer!: number;
 
   questionAnswerDuration: number[] = [];
   solutionAVG = 0;
@@ -26,15 +26,15 @@ export class AppComponent implements OnInit {
     this.firstNum =  Math.floor(Math.random()*10);
     this.secondNum =  Math.floor(Math.random()*10);
     this.result = this.firstNum + this.secondNum;
-    this.startTime = new Date().getTime();
+    this.startAnswer = new Date().getTime();
   }
 
   onUserAnswer(answerForm: NgForm) {
     if(answerForm.valid) {
       if(answerForm.value.answer === this.result) {
         this.correctAnswer = true;
-        this.endTime = new Date().getTime();
-        this.questionAnswerDuration.push((this.endTime - this.startTime)/1000)
+        this.finshAnswer = new Date().getTime();
+        this.questionAnswerDuration.push((this.finshAnswer - this.startAnswer)/1000)
         this.onUpdateSolutionAverage();
         answerForm.reset();
         this.generateRandomNums();
